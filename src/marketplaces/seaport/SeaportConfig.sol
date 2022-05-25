d2b2c87644e3854f791ea2c2447001563fc2eb99
0age <0age@protonmail.com>
2022-05-24 21:55:35 -0700

    progress a bit

--- src/marketplaces/seaport/SeaportConfig.sol
+++ src/marketplaces/seaport/SeaportConfig.sol
@@ -8,4 +8,39 @@ contract SeaportConfig {
 	address public constant approvalTarget = address(
 		0x00000000006CEE72100D161c57ADA5Bb2be1CA79
 	);
+
+	/// Get whatever needs to be signed for the given test.
+	function simpleSwapDigest() external view returns (
+		bytes32 digest,
+		bool use2098
+	) {
+    	bytes32 orderHash = bytes32(0); // TODO
+
+    	bytes32 domainSeparator = bytes32(
+        	0x0a8f10d275e6dd59030d0b1d6aa63bf27c249f4e420c727533c0f7f0f2e75261
+        );
+
+        digest = keccak256(
+        	abi.encodePacked(bytes2(0x1901), domainSeparator, orderHash)
+        );
+
+        use2098 = true;
+	}
+
+	/// Supply signature and translate into the call to make for the test.
+	function simpleSwapPayload(bytes memory signature) external view returns (
+		uint256 value,
+		bytes memory callData
+	) {
+		// Derive and return the value and calldata required for the test.
+		value = 0;
+
+		// TODO
+		bytes4 functionSelector = bytes4(0x12345678);
+		uint256 arg1 = 69420;
+
+		callData = abi.encodeWithSelector(
+			functionSelector, arg1, signature
+		);
+	}
 }
--- src/marketplaces/wyvern/WyvernConfig.sol
+++ src/marketplaces/wyvern/WyvernConfig.sol
@@ -8,4 +8,39 @@ contract WyvernConfig {
 	address public constant approvalTarget = address(
 		0x37A7996aff29966c328494d07638C7d4A710f92D
 	);
+
+	/// Get whatever needs to be signed for the given test.
+	function simpleSwapDigest() external view returns (
+		bytes32 digest,
+		bool use2098
+	) {
+    	bytes32 orderHash = bytes32(0); // TODO
+
+    	bytes32 domainSeparator = bytes32(
+        	0x72982d92449bfb3d338412ce4738761aff47fb975ceb17a1bc3712ec716a5a68
+        );
+
+        digest = keccak256(
+        	abi.encodePacked(bytes2(0x1901), domainSeparator, orderHash)
+        );
+
+        use2098 = false;
+	}
+
+	/// Supply signature and translate into the call to make for the test.
+	function simpleSwapPayload(bytes memory signature) external view returns (
+		uint256 value,
+		bytes memory callData
+	) {
+		// Derive and return the value and calldata required for the test.
+		value = 0;
+
+		// TODO
+		bytes4 functionSelector = bytes4(0x12345678);
+		uint256 arg1 = 69420;
+
+		callData = abi.encodeWithSelector(
+			functionSelector, arg1, signature
+		);
+	}
 }
