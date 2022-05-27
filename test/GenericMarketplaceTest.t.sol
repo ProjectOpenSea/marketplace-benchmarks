ee5cd0dda2308e3689dc7e0241e60c94dc8d6f25
d1ll0n <dillonkellar@protonmail.com>
2022-05-27 07:23:03 -0500

    work on test

--- test/GenericMarketplaceTest.t.sol
+++ test/GenericMarketplaceTest.t.sol
@@ -1,86 +1,123 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.14;
 
-import { DSTestPlus } from "solmate/test/utils/DSTestPlus.sol";
-
-import { SeaportConfig } from "../src/marketplaces/wyvern/SeaportConfig.sol";
+import { DSTestPlus } from "@rari-capital/solmate/src/test/utils/DSTestPlus.sol";
 import { WyvernConfig } from "../src/marketplaces/wyvern/WyvernConfig.sol";
+import { BaseMarketConfig } from "../src/BaseMarketConfig.sol";
 
-import "@rari-capital/solmate/src/tokens/ERC20.sol";
-import "@rari-capital/solmate/src/tokens/ERC721.sol";
-import "@rari-capital/solmate/src/tokens/ERC1155.sol";
+import { TestOrderPayload, TestOrderContext, TestCallParameters, TestItem20, TestItem721, TestItem1155 } from "../src/Types.sol";
 
-interface Vm {
-    function prank(address) external;
-}
+import "./tokens/TestERC20.sol";
+import "./tokens/TestERC721.sol";
+import "./tokens/TestERC1155.sol";
+import "./utils/BaseOrderTest.sol";
 
-interface ConfigInterface {
-    function marketplace() external view returns (address);
-    function approvalTarget() external view returns (address);
-    function simpleSwapPayload() external view returns (uint256 value, bytes memory callData);
-}
-
-contract TestERC20 is ERC20("Test20", "TST20", 18) {
-    function mint(address to, uint256 amount) external returns (bool) {
-        _mint(to, amount);
-        return true;
-    }
-}
-
-contract TestERC721 is ERC721("Test721", "TST721") {
-    function mint(address to, uint256 tokenId) public returns (bool) {
-        _mint(to, tokenId);
-        return true;
+contract BaseMarketplaceTester is BaseOrderTest {
+    function signDigest(address signer, bytes32 digest)
+        external
+        view
+        returns (bytes memory)
+    {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_pkOfSigner1s, payloadToSign);
+        return abi.encodePacked(r, s, v);
     }
 
-    function tokenURI(uint256) public pure override returns (string memory) {
-        return "tokenURI";
+    function setUp() public virtual override {
+        super.setUp();
     }
+
+    function _callWithParams(
+      string memory label,
+      address sender,
+      TestCallParameters memory params
+    ) internal {
+      hevm.startPrank(sender);
+      uint256 gasDelta;
+      assembly {
+        let to := mload(params)
+        let value := mload(add(params, 0x20))
+        let data := mload(mload(add(params, 0x40)))
+        let ptr := add(data, 0x20)
+        let len := mload(data)
+        let g1 := gas()
+        let success := call(
+          gas(),
+          to,
+          value,
+          ptr,
+          len,
+          0,
+          0
+        )
+        let g2 := gas()
+        gasDelta := sub(g1, g2)
+      }
+      hevm.stopPrank();
+      emit log_named_uint(string(abi.encodePacked(label, " Gas")), gasDelta);
+    }
+
+    function prepareMarketplaceTest(
+      BaseMarketConfig config
+    ) internal {
+      address target = config.approvalTarget();
+      _setApprovals(alice, target);
+      _setApprovals(bob, target);
+    }
+
+    // @todo move the approvals/minting logic into this contract
+    // and only mint/approve needed tokens
+    function benchmark_BuyOfferedERC721WithEther(
+      BaseMarketConfig config
+    )
+      resetTokenBalancesBetweenRuns
+    {
+      prepareMarketplaceTest(config);
+      TestOrderContext memory context = TestOrderContext(true, alice, bob);
+      test1155_1.mint(alice, 1, 1);
+      TestOrderPayload memory payload = config.getPayload_BuyOfferedERC1155WithERC20(
+        context,
+        TestItem1155(address(test1155_1), 1, 1),
+        100
+      );
+      
+    }
+// getPayload_BuyOfferedERC1155WithEther
+// getPayload_BuyOfferedERC721WithERC20
+// getPayload_BuyOfferedERC1155WithERC20
+// getPayload_BuyOfferedERC20WithERC721
+// getPayload_BuyOfferedERC20WithERC1155
 }
 
-contract TestERC1155 is ERC1155 {
-    function mint(
-        address to,
-        uint256 tokenId,
-        uint256 amount
-    ) public returns (bool) {
-        _mint(to, tokenId, amount, "");
-        return true;
-    }
-
-    function uri(uint256) public pure override returns (string memory) {
-        return "uri";
-    }
-}
-
-contract GenericMarketplaceTest is DSTestPlus {
-    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
-
+contract GenericMarketplaceTest is DSTestPlus, BaseOrderTest {
     Config seaport;
     Config wyvern;
     TestERC20 erc20;
     TestERC721 erc721;
     TestERC1155 erc1155;
 
-    address constant seller = address(
-        0x0734d56DA60852A03e2Aafae8a36FFd8c12B32f1
-    );
-
-    address constant buyer = address(
-        0x939C8d89EBC11fA45e576215E2353673AD0bA18A
-    );
-
-    function setUp() public {
-        seaport = Config(address(new SeaportConfig()));
-        wyvern = Config(address(new WyvernConfig()));
-        erc20 = new TestERC20();
-        erc721 = new TestERC721();
-        erc1155 = new TestERC1155();
+    function signDigest(address signer, bytes32 digest)
+        external
+        view
+        returns (bytes memory)
+    {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_pkOfSigner1s, payloadToSign);
     }
 
-    function _prepareTest(
-        Config target
-    ) internal returns (address to, uint256 value, bytes memory callData) {
+    function setUp() public virtual override {
+        super.setUp();
+        seaport = Config(address(new SeaportConfig()));
+    }
+
+
+
+    function _prepareTest(Config target)
+        internal
+        returns (
+            address to,
+            uint256 value,
+            bytes memory callData
+        )
+    {
         uint256 tokenId = 100;
 
         erc721.mint(seller, tokenId);
@@ -94,10 +131,7 @@ contract GenericMarketplaceTest is DSTestPlus {
 
         (bytes32 payloadToSign, bool use2098) = target.supplyPayloadToSign();
 
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
-            _pkOfSigner,
-            payloadToSign
-        );
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_pkOfSigner, payloadToSign);
 
         bytes memory signature;
         if (use2098) {
@@ -120,7 +154,7 @@ contract GenericMarketplaceTest is DSTestPlus {
         (address to, bytes memory callData) = _prepareTest(seaport);
 
         vm.prank(buyer);
-        (bool ok, ) = to.call{value}(callData);
+        (bool ok, ) = to.call{ value: value }(callData);
 
         require(ok);
     }
@@ -129,7 +163,7 @@ contract GenericMarketplaceTest is DSTestPlus {
         (address to, bytes memory callData) = _prepareTest(wyvern);
 
         vm.prank(buyer);
-        (bool ok, ) = to.call{value}(callData);
+        (bool ok, ) = to.call{ value: value }(callData);
 
         require(ok);
     }
---
+++ test/utils/BaseOrderTest.sol
@@ -0,0 +1,199 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
+import { DSTestPlus } from "@rari-capital/solmate/src/test/utils/DSTestPlus.sol";
+import { stdStorage, StdStorage } from "forge-std/Test.sol";
+import { TestERC1155 } from "../tokens/TestERC1155.sol";
+import { TestERC20 } from "../tokens/TestERC20.sol";
+import { TestERC721 } from "../tokens/TestERC721.sol";
+import { ArithmeticUtil } from "./ArithmeticUtil.sol";
+
+contract BaseOrderTest is DSTestPlus {
+    using stdStorage for StdStorage;
+    using ArithmeticUtil for uint256;
+    using ArithmeticUtil for uint128;
+    using ArithmeticUtil for uint120;
+
+    uint256 constant MAX_INT = ~uint256(0);
+
+    uint256 internal alicePk = 0xa11ce;
+    uint256 internal bobPk = 0xb0b;
+    uint256 internal calPk = 0xca1;
+    address payable internal alice = payable(hevm.addr(alicePk));
+    address payable internal bob = payable(hevm.addr(bobPk));
+    address payable internal cal = payable(hevm.addr(calPk));
+
+    TestERC20 internal token1;
+    TestERC20 internal token2;
+    TestERC20 internal token3;
+
+    TestERC721 internal test721_1;
+    TestERC721 internal test721_2;
+    TestERC721 internal test721_3;
+
+    TestERC1155 internal test1155_1;
+    TestERC1155 internal test1155_2;
+    TestERC1155 internal test1155_3;
+
+    address[] allTokens;
+    TestERC20[] erc20s;
+    TestERC721[] erc721s;
+    TestERC1155[] erc1155s;
+    address[] accounts;
+    mapping(address => uint256) internal privateKeys;
+
+    uint256 internal globalTokenId;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event TransferSingle(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256 id,
+        uint256 value
+    );
+
+    struct RestoreERC20Balance {
+        address token;
+        address who;
+    }
+
+    /**
+    @dev top up eth of this contract to uint128(MAX_INT) to avoid fuzz failures
+     */
+    modifier topUp() {
+        hevm.deal(address(this), uint128(MAX_INT));
+        _;
+    }
+
+    /**
+     * @dev hook to record storage writes and reset token balances in between differential runs
+     */
+
+    modifier resetTokenBalancesBetweenRuns() {
+        _;
+        _resetTokensAndEthForTestAccounts();
+    }
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        hevm.label(alice, "alice");
+        hevm.label(bob, "bob");
+        hevm.label(cal, "cal");
+        hevm.label(address(this), "testContract");
+
+        privateKeys[alice] = alicePk;
+        privateKeys[bob] = bobPk;
+        privateKeys[cal] = calPk;
+
+        _deployTestTokenContracts();
+        accounts = [alice, bob, cal, address(this)];
+        erc20s = [token1, token2, token3];
+        erc721s = [test721_1, test721_2, test721_3];
+        erc1155s = [test1155_1, test1155_2, test1155_3];
+        allTokens = [
+            address(token1),
+            address(token2),
+            address(token3),
+            address(test721_1),
+            address(test721_2),
+            address(test721_3),
+            address(test1155_1),
+            address(test1155_2),
+            address(test1155_3)
+        ];
+
+        // allocate funds and tokens to test addresses
+        globalTokenId = 1;
+    }
+
+    /**
+    @dev deploy test token contracts
+     */
+    function _deployTestTokenContracts() internal {
+        token1 = new TestERC20();
+        token2 = new TestERC20();
+        token3 = new TestERC20();
+        test721_1 = new TestERC721();
+        test721_2 = new TestERC721();
+        test721_3 = new TestERC721();
+        test1155_1 = new TestERC1155();
+        test1155_2 = new TestERC1155();
+        test1155_3 = new TestERC1155();
+        hevm.label(address(token1), "token1");
+        hevm.label(address(test721_1), "test721_1");
+        hevm.label(address(test1155_1), "test1155_1");
+
+        emit log("Deployed test token contracts");
+    }
+
+    function _setApprovals(address _owner, address _target) internal {
+        hevm.startPrank(_owner);
+        for (uint256 i = 0; i < erc20s.length; i++) {
+            erc20s[i].approve(_target, MAX_INT);
+        }
+        for (uint256 i = 0; i < erc721s.length; i++) {
+            erc721s[i].setApprovalForAll(_target, true);
+        }
+        for (uint256 i = 0; i < erc1155s.length; i++) {
+            erc1155s[i].setApprovalForAll(_target, true);
+        }
+
+        hevm.stopPrank();
+    }
+
+    /**
+     * @dev reset written token storage slots to 0 and reinitialize uint128(MAX_INT) erc20 balances for 3 test accounts and this
+     */
+    function _resetTokensAndEthForTestAccounts() internal {
+        _resetTokensStorage();
+        _restoreERC20Balances();
+        _restoreEthBalances();
+    }
+
+    function _restoreEthBalances() internal {
+        for (uint256 i = 0; i < accounts.length; i++) {
+            hevm.deal(accounts[i], uint128(MAX_INT));
+        }
+    }
+
+    function _resetTokensStorage() internal {
+        for (uint256 i = 0; i < allTokens.length; i++) {
+            _resetStorage(allTokens[i]);
+        }
+    }
+
+    /**
+     * @dev restore erc20 balances for all accounts
+     */
+    function _restoreERC20Balances() internal {
+        for (uint256 i = 0; i < accounts.length; i++) {
+            _restoreERC20BalancesForAddress(accounts[i]);
+        }
+    }
+
+    /**
+     * @dev restore all erc20 balances for a given address
+     */
+    function _restoreERC20BalancesForAddress(address _who) internal {
+        for (uint256 i = 0; i < erc20s.length; i++) {
+            _restoreERC20Balance(RestoreERC20Balance(address(erc20s[i]), _who));
+        }
+    }
+
+    /**
+     * @dev reset token balance for an address to uint128(MAX_INT)
+     */
+    function _restoreERC20Balance(
+        RestoreERC20Balance memory restoreErc20Balance
+    ) internal {
+        stdstore
+            .target(restoreErc20Balance.token)
+            .sig("balanceOf(address)")
+            .with_key(restoreErc20Balance.who)
+            .checked_write(uint128(MAX_INT));
+    }
+
+    receive() external payable virtual {}
+}
