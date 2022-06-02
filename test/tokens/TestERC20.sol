ca6187c12816609082b8ee1d52121de12ed1eddc
0age <0age@protonmail.com>
2022-06-02 13:16:46 -0700

    update token locations

--- test/tokens/TestERC20.sol
+++ test/tokens/TestERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.14;
 
-import "solmate/src/tokens/ERC20.sol";
+import "solmate/tokens/ERC20.sol";
 
 contract TestERC20 is ERC20("Test20", "TST20", 18) {
     function mint(address to, uint256 amount) external returns (bool) {
