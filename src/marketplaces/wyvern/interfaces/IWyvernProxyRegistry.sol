288fc6f3dd4a4e334a4cfc6b05c0ab03fdc3d2b7
arr00 <13561405+arr00@users.noreply.github.com>
2022-06-20 14:57:51 -0400

    clean results

--- src/marketplaces/wyvern/interfaces/IWyvernProxyRegistry.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.13;
-
-interface IWyvernProxyRegistry {
-    function registerProxy() external returns (address);
-
-    function proxies(address) external returns (address);
-}
